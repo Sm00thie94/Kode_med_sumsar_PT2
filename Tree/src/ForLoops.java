8df1c4301b284f63db1f4adc9ef962add4074acb
Sm00thie94 <96887356+Sm00thie94@users.noreply.github.com>
2022-01-09 03:04:19 +0100

    If statement: 127
    Updated so the tabel would look more clean and organized, by adding an if statement that would add an extra space, if a number is a single digit.

--- Tree/src/ForLoops.java
+++ Tree/src/ForLoops.java
@@ -124,6 +124,9 @@ public class ForLoops {
         }
         for (int x = 1; x <= input; x++) {
             for (int y = 1; y <= input; y++) {
+                if (x * y < 10) {
+                    System.out.print(" ");
+                }
                 System.out.print(x * y + " ");
             }
             System.out.println();
